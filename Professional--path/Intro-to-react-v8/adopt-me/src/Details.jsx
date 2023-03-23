0119066b2222e1cdc7275b425caadca8f5c7543a
ziadhassan8 <ziadhamdy268@gmail.com>
2023-03-23 10:32:30 +0200

    Used functional components

---
+++ Professional--path/Intro-to-react-v8/adopt-me/src/Details.jsx
@@ -0,0 +1,39 @@
+import { useParams } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import fetchPet from "./fetchPet";
+import Carousel from "./Carousel";
+
+const Details = () => {
+  const { id } = useParams();
+  const results = useQuery(["details", id], fetchPet); // key : value for cache else run fetchPet
+
+  if (results.isError) {
+    return <h1>Oh no!</h1>;
+  }
+  // for the first load
+  if (results.isLoading) {
+    return (
+      <div className="loading-pane">
+        <h2 className="loader">🌀</h2>
+      </div>
+    );
+  }
+
+  const pet = results.data.pets[0];
+
+  return (
+    <div className="details">
+      <Carousel images={pet.images} />
+      <div>
+        <h1>{pet.name}</h1>
+        <h2>
+          {pet.animal} — {pet.breed} — {pet.city}, {pet.state}
+          <button>Adopt {pet.name}</button>
+          <p>{pet.description}</p>
+        </h2>
+      </div>
+    </div>
+  );
+};
+
+export default Details;
--- Professional--path/Intro-to-react-v8/adopt-me/src/SearchParams.jsx
+++ Professional--path/Intro-to-react-v8/adopt-me/src/SearchParams.jsx
@@ -1,21 +1,69 @@
 import { useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+import useBreedList from "./useBreedList";
+import fetchSearch from "./fetchSearch";
+import Results from "./Results";
+const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 
 const SearchParams = () => {
-  const [location, setLocation] = useState("");
+  const [requestParams, setRequestParams] = useState({
+    location: "",
+    animal: "",
+    breed: "",
+  });
+  const [animal, setAnimal] = useState("");
+  const [breeds] = useBreedList(animal);
+
+  const results = useQuery(["search", requestParams], fetchSearch);
+  const pets = results?.data?.pets ?? [];
+
   return (
     <div className="search-params">
-      <form>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault(); // to not make it full form submit
+          const formData = new FormData(e.target);
+          const obj = {
+            animal: formData.get("animal") ?? "",
+            breed: formData.get("breed") ?? "",
+            location: formData.get("location") ?? "",
+          };
+          setRequestParams(obj);
+        }}
+      >
         <label htmlFor="location">
           Location
-          <input
-            onChange={(e) => setLocation(e.target.value)}
-            id="location"
-            value={location}
-            placeholder="Location"
-          />
+          <input name="location" id="location" placeholder="Location" />
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={(e) => {
+              setAnimal(e.target.value);
+            }}
+          >
+            <option />
+            {ANIMALS.map((animal) => (
+              <option key={animal}>{animal}</option>
+            ))}
+          </select>
+        </label>
+        <label htmlFor="breed">
+          Breed
+          <select id="breed" name="breed" disabled={breeds.length === 0}>
+            <option />
+            {breeds.map((breed) => (
+              <option key={breed} value={breed}>
+                {breed}
+              </option>
+            ))}
+          </select>
         </label>
         <button>Submit</button>
       </form>
+      <Results pets={pets} />
     </div>
   );
 };
